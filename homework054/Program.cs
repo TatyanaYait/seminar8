e5e6e81e407ce1e413635e5b43025dcd0e9a50f1
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-16 22:39:23 +0600

    упорядочивание элеменов строк массива по убыванию

---
+++ homework054/Program.cs
@@ -0,0 +1,63 @@
+﻿int[,] GetArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); ++i)
+    {
+        for (int j = 0; j < arr.GetLength(1); ++j)
+        {
+            Console.Write("  " + arr[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите число строк массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(m, n);
+PrintArray(array);
+Console.WriteLine();
+
+
+
+for (int i = 0; i < array.GetLength(0); ++i)
+{
+    for (int j = 0; j < array.GetLength(1); ++j)
+    {
+        for (int k = j + 1; k < array.GetLength(1); k++)
+        {
+            if (array[i, j] < array[i, k])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = temp;
+            }
+
+        }
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.WriteLine();
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write("  " + array[i, j]);
+    }
+}
